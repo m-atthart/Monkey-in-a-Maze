e70e19055c5927c5cc4ad0b93ed2c5cb8216e851
snarkiergoblin <33573971+snarkiergoblin@users.noreply.github.com>
2019-08-14 15:27:21 +0100

    Syntax fixes. Update random coordinate generation

--- maze-generation.py
+++ maze-generation.py
@@ -6,27 +6,27 @@ import random
 
 
 def iterate():
-    #Find random closed (wall) cell, which is an even coordinate pair. generate a direction to go in
+    #Find random closed (wall) cell, which is an odd coordinate pair. generate a direction to go in
     #I is vertical axis, J is horizontal axis. direction=1 is vertical, direction=0 is horizontal
-    random_i = int(random.randint(0,height//2)*2)
-    random_j = int(random.randint(0,width//2)*2)
+    random_i=random.randrange(1, height, 2)
+    random_j=random.randrange(1, width, 2)
     direction=random.randint(0, 1)
-    
     #check if the wall has already been opened
     if maze[random_i][random_j] != 0:
+    #pass this time
         pass
-    
+    #check if the two cells on vertical are in the same group
     if direction==1:
         if maze[random_i - 1][random_j] != maze[random_i+1][random_j]:
             #set a holder variable for the top value, which will be passed down to the mid/bottom values
             top_value_holder=maze[random_i+1][random_j]
             for k in range(-1, 2):
-                #set all three to the top value to help the maze grow
                 maze[random_i + k][random_j] = top_value_holder
-    if direction==-0
-        if maze[random_i][random_j - 1] != maze[random_i][random_j+1]:
-            #set a holder variable for the left value, which will be passed down to the mid/bottom values
-            top_value_holder=maze[random_i][random_j -1]
-            for k in range(-1, 2):
-                #set all three to the top value to help the maze grow
-                maze[random_i][random_j + k] = top_value_holder
+        if direction==0:
+            if maze[random_i][random_j - 1] != maze[random_i][random_j+1]:
+                #set a holder variable for the left value, which will be passed down to the mid/bottom values
+                top_value_holder=maze[random_i][random_j -1]
+                for k in range(-1, 2):
+                    maze[random_i][random_j + k] = top_value_holder
+    for i in maze:
+        print(i)
