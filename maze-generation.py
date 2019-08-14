419e509aadcc74a2b466d91d21f17c34ff5aa2fb
snarkiergoblin <33573971+snarkiergoblin@users.noreply.github.com>
2019-08-14 20:35:31 +0100

    fix typo

--- maze-generation.py
+++ maze-generation.py
@@ -7,7 +7,7 @@ import random
 open_counter=1
 for i in range(5):
     for j in range(5):
-        if i%2 != 0 and j%2 != 0:
+        if i%2 == 0 and j%2 == 0:
             maze[i][j]=open_counter
             open_counter+=1
 
