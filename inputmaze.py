0005158b0cf2c167e485da05aa4dcbc36e896aec
Alexia <alexia.georgiou53@gmail.com>
2019-08-15 14:39:30 +0100

    Add keyboard input detector

---
+++ inputmaze.py
@@ -0,0 +1,143 @@
+# Array-Backed Grids_Test
+
+import pygame
+from pygame.locals import *
+import numpy as np
+
+# Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BROWN = (222, 184, 135)
+GREEN = (107, 142, 35)
+BLUE = (135, 206, 250)
+
+
+# sides of a grid
+WIDTH = 15
+HEIGHT = 15
+
+grid = []
+for row in range(34):
+    # Add an empty array that will hold each cell in this row
+    grid.append([])
+    for column in range(21):
+        grid[row].append(0)  # Append a cell
+
+x, y = 21, 34
+
+# set Window size
+WINDOW_SIZE = [315, 510]  # (width,height)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+# initialize pygame
+pygame.init()
+
+# Set title of screen
+pygame.display.set_caption("Monkey in the Maze")
+
+done = False  # close button
+
+clock = pygame.time.Clock()  # how fast the screen takes to update
+
+
+maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 3, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+grid = maze
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+
+    # Set the screen background
+    screen.fill(WHITE)
+
+    # Draw the grid
+    # for row in range(34):
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            # for column in range(21):
+            color = WHITE
+            # set colors for the elements
+            # wall = 1
+            if grid[row][column] == 1:
+                color = BROWN
+            # character is 2
+            if grid[row][column] == 2:
+                color = GREEN
+                pos = grid[row][column]
+                posl = grid[row-1][column]
+            # finishing point
+            if grid[row][column] == 3:
+                color = BLUE
+
+            pygame.draw.rect(screen, color,
+                             [WIDTH * column, HEIGHT * row, WIDTH, HEIGHT])
+
+
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+    if keys[K_ESCAPE]:
+        done = True
+
+    if keys[K_LEFT]:
+        print("left")
+        # player.move_left()
+ 
+    elif keys[K_RIGHT]:
+        print("right")
+        # player.move_right()
+        pygame.draw.rect(screen, GREEN,
+                         [WIDTH * column, HEIGHT * row, WIDTH + 1, HEIGHT])
+    elif keys[K_UP]:
+        print("up")
+        pygame.draw.rect(screen, GREEN,
+                         [WIDTH * column, HEIGHT * row, WIDTH, HEIGHT + 1])
+
+    elif keys[K_DOWN]:
+        print("down")
+        pygame.draw.rect(screen, GREEN,
+                         [WIDTH * column, HEIGHT * row, WIDTH, HEIGHT-1])
+
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    pygame.display.flip()
+
+
+
+# exit.
+pygame.quit()
